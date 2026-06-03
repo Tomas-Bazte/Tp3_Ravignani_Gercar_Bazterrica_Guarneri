4473ea774ef4b069f37b228bb104fa4d3f561445
thiagogercar <tgercarprieto@udesa.edu.ar>
2026-06-02 22:03:31 -0300

    Avances significativos de class PacMan y algunas correciones de detalles en class Mapa

--- Mapa.py
+++ Mapa.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+from entidades import PacMan
 
 pg.init()
 def Cargar_Mapa(Ruta: str) -> list:
@@ -24,12 +25,21 @@ class pared (pg.sprite.Sprite):
 class puntitos (pg.sprite.Sprite):
     def __init__ (self,x,y,Super=False):
         super().__init__()
+        self.es_power_pellet = Super
         radio = (7 if Super else 3)
         tamaño = radio*2
         self.image = pg.Surface((tamaño,tamaño),pg.SRCALPHA)
         pg.draw.circle(self.image,(255,255,255),(radio,radio),radio)
         self.rect = self.image.get_rect()
         self.rect.center = (x+12,y+12)
+    
+    def flash_power_pellet(self):
+        if self.es_power_pellet:
+            tiempo = pg.time.get_ticks()
+            if (tiempo // 200) % 2 == 0:
+                self.image.set_alpha(255)
+            else:
+                self.image.set_alpha(0)
 
 class tuneles (pg.sprite.Sprite):
     def __init__(self):
@@ -59,24 +69,73 @@ def Dibujar_Mapa(pantalla, mapa : list, tamaño_casillero = 24) -> None:
                 
     return grupo_Paredes, grupo_puntos, Pos_Pm
 
-TILE = 24
 
-mapa = Cargar_Mapa("Mapa.txt")
+pg.init()
+pg.mixer.init()
 
-ANCHO = len(mapa[0]) * TILE
-ALTO = len(mapa) * TILE
+TILE_SIZE = 24
+ANCHO = 28 * TILE_SIZE
+ALTO = 31 * TILE_SIZE
 
 pantalla = pg.display.set_mode((ANCHO, ALTO))
-grupo_Paredes, grupo_puntos, Pos_Pm = Dibujar_Mapa(pantalla, mapa, TILE)
+pg.display.set_caption("Test PacMan con mapa")
 
-while True:
+reloj = pg.time.Clock()
+
+mapa = Cargar_Mapa("mapa.txt")
+grupo_paredes, grupo_puntos, Pos_Pm = Dibujar_Mapa(pantalla, mapa, TILE_SIZE)
+
+pacman = PacMan(Pos_Pm[0] + TILE_SIZE // 2, Pos_Pm[1] + TILE_SIZE // 2)
+pacman.cargar_frames_muerte()
+
+jugando = True
+
+while jugando:
+    # dt = segundos desde el último frame.
+    # Se usa para que PacMan se mueva en tiles/segundo.
+    dt = reloj.tick(60) / 1000
 
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
-            pg.quit()
-            sys.exit()
+            jugando = False
+
+        if evento.type == pg.KEYDOWN:
+            if evento.key == pg.K_m and pacman.estado != "muriendo":
+                pacman.iniciar_muerte()
+            elif pacman.estado != "muriendo":
+                pacman.cambiar_direccion(evento.key)
+
+    if pacman.estado == "muriendo":
+        if pacman.actualizar_muerte():
+            pacman.reiniciar_posicion(
+                Pos_Pm[0] + TILE_SIZE // 2,
+                Pos_Pm[1] + TILE_SIZE // 2
+            )
+    else:
+        pacman.mover(dt, TILE_SIZE)
+        pacman.actualizar_animacion()
+        pacman.actualizar_super()
+
+        hitbox_pm = pacman.obtener_hitbox()
+
+        for punto in grupo_puntos.copy():
+            if hitbox_pm.colliderect(punto.rect):
+                if punto.es_power_pellet:
+                    pacman.comer_power_pellet()
+                else:
+                    pacman.comer_punto()
+
+                punto.kill()
+
+    for punto in grupo_puntos:
+        punto.flash_power_pellet()
 
     pantalla.fill((0, 0, 0))
+
+    grupo_paredes.draw(pantalla)
     grupo_puntos.draw(pantalla)
-    grupo_Paredes.draw(pantalla)
+    pacman.dibujar(pantalla)
+
     pg.display.flip()
+
+pg.quit()
--- entidades.py
+++ entidades.py
@@ -1,37 +1,56 @@
 import pygame as pg
 import math
 
+TILE_SIZE = 24
+
 class Criatura:
     def __init__(self,x,y,velocidad):
         self.x = x
         self.y = y
-        self.velocidad = velocidad
+        self.velocidad = velocidad # tiles/segundo
         self.direccion = "quieto"
-        self.radio = 18 # Radio de pixeles por default, tomando en cuenta el size del tile como 24x24 pixeles
+        self.radio = 9.5 # Radio de pixeles por default, tomando en cuenta el size del tile como 24x24 pixeles
     
-    def mover(self):
+    def mover(self, dt, tile_size=24):  # dt = tiempo transcurrido desde el ultimo frame (en segundos). Permite usar tiles/segundos
+        desplazamiento = self.velocidad * tile_size * dt
         if self.direccion == "derecha":
-            self.x += self.velocidad
+            self.x += desplazamiento
         elif self.direccion == "izquierda":
-            self.x -= self.velocidad
+            self.x -= desplazamiento
         elif self.direccion == "arriba":
-            self.y -= self.velocidad
+            self.y -= desplazamiento
         elif self.direccion == "abajo":
-            self.y += self.velocidad
+            self.y += desplazamiento
     
     def obtener_hitbox(self):
         return pg.Rect(self.x - self.radio, self.y - self.radio, self.radio * 2, self.radio * 2)
          
 class PacMan (Criatura):
-    velocidad_normal = 0.80
-    velocidad_super = 0.90
+    velocidad_normal = 6 # 80 % de 7.5 - tiles/segundo
+    velocidad_super = 6.75 # 90 % de 7.5
     def __init__(self, x, y):
         super().__init__(x, y, PacMan.velocidad_normal)
         self.vidas = 3
         self.puntaje = 0
+        self.estado = "normal" # normal, muriendo
         self.frame_animacion = 0 # frame_animacion = 0 - boca casi cerrada, frame_animacion = 5  - boca media abierta, frame_animacion = 10 - boca muy abierta
+        self.frame_min_boca = 0
+        self.frame_max_boca = 10
+        self.velocidad_animacion_boca = 1
+        self.frames_muerte = []
+        self.frame_muerte_actual = 0
+        self.tiempo_ultimo_frame_muerte = 0
+        self.duracion_entre_frames_muerte = 2500 // 13 # 2500 milisegundos = 2.5 segundos entre 13 frames, como pacman original
         self.boca_abriendo = True # True: boca se esta abriendo, False: boca se esta cerrando
         self.vida_extra_dada = False # A los 10k puntos se devuelve una vida, pasa solamente una vez.
+        self.modo_super = False
+        self.tiempo_super_inicio = 0
+        self.duracion_super = 6000 # milisegundos = 6 segundos
+        self.sonido_muerte = pg.mixer.Sound("sonidos_pacman/death_0.wav")
+        self.sonido_dot_0 = pg.mixer.Sound("sonidos_pacman/eat_dot_0.wav")
+        self.sonido_dot_1 = pg.mixer.Sound("sonidos_pacman/eat_dot_1.wav")
+        self.sonido_fright = pg.mixer.Sound("sonidos_pacman/fright.wav")
+        self.alternar_sonido_dot = 0
         
     def cambiar_direccion(self, tecla):
         if tecla == pg.K_RIGHT:
@@ -45,28 +64,38 @@ class PacMan (Criatura):
             
     def dibujar(self, pantalla):
         centro = (int(self.x), int(self.y))
+        if self.estado == "muriendo":
+            if self.frame_muerte_actual < len(self.frames_muerte):
+                imagen = self.frames_muerte[self.frame_muerte_actual]
+                imagen = pg.transform.rotate(imagen, 270) # La animacion de la muerte es con la boca mirando para arriba
+                rect = imagen.get_rect(center = centro)
+                pantalla.blit(imagen, rect)
+            return
         pg.draw.circle(pantalla, (255, 255, 0), centro, self.radio)
-        apertura = 4 + self.frame_animacion # la boca siempre esta abierta por eso el 4
+        if self.frame_animacion<1:
+            return
         if self.direccion == "derecha" or self.direccion == "quieto":
-            boca = [centro, (self.x + self.radio, self.y - apertura), (self.x + self.radio, self.y + apertura)]
+            boca = [centro, (self.x + self.radio, self.y - self.frame_animacion), (self.x + self.radio, self.y + self.frame_animacion)]
         elif self.direccion == "izquierda":
-            boca = [centro, (self.x - self.radio, self.y - apertura), (self.x - self.radio, self.y + apertura)]
+            boca = [centro, (self.x - self.radio, self.y - self.frame_animacion), (self.x - self.radio, self.y + self.frame_animacion)]
         elif self.direccion == "arriba":
-            boca = [centro, (self.x - apertura, self.y - self.radio), (self.x + apertura, self.y - self.radio)]
+            boca = [centro, (self.x - self.frame_animacion, self.y - self.radio), (self.x + self.frame_animacion, self.y - self.radio)]
         elif self.direccion == "abajo":
-            boca = [centro, (self.x - apertura, self.y + self.radio), (self.x + apertura, self.y + self.radio)]
+            boca = [centro, (self.x - self.frame_animacion, self.y + self.radio), (self.x + self.frame_animacion, self.y + self.radio)]
         pg.draw.polygon(pantalla, (0, 0, 0), boca)
     
     def actualizar_animacion(self):
         if self.direccion == "quieto":
             return
         if self.boca_abriendo:
-            self.frame_animacion += 1
-            if self.frame_animacion >= 10:
+            self.frame_animacion += self.velocidad_animacion_boca
+            if self.frame_animacion >= self.frame_max_boca:
+                self.frame_animacion = self.frame_max_boca
                 self.boca_abriendo = False
         else:
-            self.frame_animacion -= 1
-            if self.frame_animacion <= 0:
+            self.frame_animacion -= self.velocidad_animacion_boca
+            if self.frame_animacion <= self.frame_min_boca:
+                self.frame_animacion = self.frame_min_boca
                 self.boca_abriendo = True
     
     def sumar_puntos(self,puntos):
@@ -84,11 +113,23 @@ class PacMan (Criatura):
         return self.vidas > 0
 
     def activar_super(self):
+        self.modo_super = True
+        self.tiempo_super_inicio = pg.time.get_ticks()
         self.velocidad = PacMan.velocidad_super
+        self.sonido_fright.play(loops=-1)
     
     def desactivar_super(self):
+        self.modo_super = False
         self.velocidad = PacMan.velocidad_normal
+        self.sonido_fright.stop()
     
+    def actualizar_super(self):
+        if self.modo_super:
+            tiempo_actual = pg.time.get_ticks()
+            
+            if tiempo_actual - self.tiempo_super_inicio >= self.duracion_super:
+                self.desactivar_super()
+                
     def reiniciar_posicion(self, x, y): # Volver a posición inicial
         self.x = x
         self.y = y
@@ -101,8 +142,43 @@ class PacMan (Criatura):
     
     def comer_punto(self):
         self.sumar_puntos(10)
+        if self.alternar_sonido_dot == 0:
+            self.sonido_dot_0.play()
+            self.alternar_sonido_dot = 1
+        else:
+            self.sonido_dot_1.play()
+            self.alternar_sonido_dot = 0
     
     def comer_power_pellet(self):
         self.sumar_puntos(50)
         self.activar_super()
-      
+    
+    def iniciar_muerte(self):
+        self.estado = "muriendo"
+        self.direccion = "quieto"
+        self.frame_muerte_actual = 0
+        self.tiempo_ultimo_frame_muerte = pg.time.get_ticks()
+        self.sonido_muerte.play()
+    
+    def actualizar_muerte(self):
+        if self.estado != "muriendo":
+            return False
+        tiempo_ahora = pg.time.get_ticks()
+        if tiempo_ahora - self.tiempo_ultimo_frame_muerte >= self.duracion_entre_frames_muerte:
+            self.frame_muerte_actual += 1
+            self.tiempo_ultimo_frame_muerte = tiempo_ahora
+        if self.frame_muerte_actual >= len(self.frames_muerte):
+            self.perder_vida()
+            self.estado = "normal"
+            return True
+        return False
+    
+    def cargar_frames_muerte(self):
+        self.frames_muerte = []
+
+        for i in range(13):
+            imagen = pg.image.load(f"pacman_muerte/pacman_death_{i:02}.png").convert_alpha()
+            imagen = pg.transform.scale(imagen, (self.radio * 2.8, self.radio * 2.8)) # 2.8 es una escala aproximada para que la imagen quede del mismo tamaño que el PacMan
+            self.frames_muerte.append(imagen)
+    
+   
